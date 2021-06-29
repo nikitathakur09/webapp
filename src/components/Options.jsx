9739723ecec8838f142fa5da6f971adaf12f88f9
nikitathakur09 <70997038+nikitathakur09@users.noreply.github.com>
2021-06-29 22:30:04 +0530

    Add files via upload

---
+++ src/components/Options.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Options = ({ children }) => {
+    return (
+        <div>
+            hello
+            {children}
+        </div>
+    )
+}
+
+export default Options
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import { ContextProvider } from './SocketContext';
+
+import './styles.css';
+
+ReactDOM.render(
+    
+    <App />
+    ,
+   document.getElementById('root'),
+   );
